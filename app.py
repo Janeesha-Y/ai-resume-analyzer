0bbc2ae0ce11ddde48816b8043b5b8499295024a
Janeesha-Y <janeeshay06@gmail.com>
2026-04-16 21:44:02 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from pypdf import PdfReader
+import PyPDF2
 import matplotlib.pyplot as plt
 import re
 import os
@@ -57,7 +57,7 @@ st.sidebar.success("👩‍💻 Developed by Janeesha Y")
 
 # ---------------- FUNCTION ----------------
 def extract_text(file):
-    reader = PdfReader(file)
+    reader = PyPDF2.PdfReader(file)
     text = ""
     for p in reader.pages:
         if p.extract_text():
